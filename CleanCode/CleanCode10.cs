cf8910f5a0f8779284518b0917acfb63409e5564
Briotar <vladimirklevtsovmain@gmail.com>
2022-06-29 13:22:31 +0300

    Clean Code 10

---
+++ CleanCode/CleanCode10.cs
@@ -0,0 +1,10 @@
+﻿
+class Weapon
+{
+    private int _bullets;
+    private readonly int _bulletsPerShot = 1;
+
+    public bool CanShoot() => _bullets >= _bulletsPerShot;
+
+    public void Shoot() => _bullets -= _bulletsPerShot;s
+}
