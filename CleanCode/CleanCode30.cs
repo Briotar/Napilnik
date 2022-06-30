e0e619b78868e821370311c06794e97b628a5b0c
Briotar <vladimirklevtsovmain@gmail.com>
2022-06-30 17:34:39 +0300

    CleanCode30

---
+++ CleanCode/CleanCode30.cs
@@ -0,0 +1,11 @@
+﻿public void ActivateEffects()
+{
+    _enable = true;
+    _effects.StartEnableAnimation();
+}
+
+public void DeactivateEffects()
+{
+    _enable = false;
+    _pool.Free(this);
+}
